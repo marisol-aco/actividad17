498c8df9690e0889e3c79841b9bb4efd0becc6c7
sol <solacosta619@gmail.com>
2024-06-14 01:13:52 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Marisol Martínez Acosta</p>'; 
+    echo '<p>Colonia: Los Pinos</p>'; 
+    echo '<p>Calle: 7</p>'; 
+    echo '<p>Número de casa: #1026</p>'; 
+    echo '<p>Municpio: Saltillo</p>'; 
+    echo '<p>Telefono: 8443537385</p>'; 
+    ?> 
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,8 @@
+inicio del proceso
+imprimir "Marisol Martínez Acosta"
+imprimir "Colonia: Los pinos"
+imprimir "Calle: 7"
+imprimir "Número de casa: #1026"
+imprimir "Municpio: Saltillo"
+imprimir "Telefono: 8443537385" 
+fin del proceso
